d2f26e0afeaf13eecee134ad76741b0067156b4e
Isaac Rahel <isaac_rahel@live.com>
2022-09-27 17:31:06 -0300

    fix: fixing icon

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './routes/App'
 
-{/*TODO: error page*/}
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
